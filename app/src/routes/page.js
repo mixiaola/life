fcaa8e888d56d0b01cc45fb740fb692308af51cb
peijiancheng <peijiancheng@didichuxing.com>
2018-07-04 18:10:32 +0800

    修改新增页面入口判读登陆

--- app/src/routes/page.js
+++ app/src/routes/page.js
@@ -1,11 +1,20 @@
 const router = require('koa-router')()
+//首页入口
 router.get('/', async (ctx, next) => {
+	if (ctx.cookies.get('isLogin')){
+		await next()
+	} else {
+		await ctx.render('login')
+	}
+}, async (ctx, next) => {
 	let title = 'koa2'
-    await ctx.render('index', { title })
+    await ctx.render('index')
 })
+
+//登录页面入口
 router.get('/pagelogin', async (ctx, next) => {
 	let title = 'koa2'
-    await ctx.render('login', { title })
+    await ctx.render('login')
 })
 
 module.exports = router
