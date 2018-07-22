df741e609932b5c53597535c7edfc37e9985dd9b
peijiancheng <peijiancheng@didichuxing.com>
2018-07-22 12:43:19 +0800

    修改图片为url格式

--- src/page/index/component/query/index.vue
+++ src/page/index/component/query/index.vue
@@ -1,18 +1,59 @@
-<template><div>query</div></template>
+<template>
+	<div class='container'>
+		<div class='titleUser'>总体单个用户量</div>
+		<div class='titleUserNumber'>{{userNumber}}</div>
+		<div class='titleTicket'>优惠总使用数量</div>
+		<div class='titleTicketNumber'>{{ticketNumber}}</div>
+	</div>
+</template>
 <script>
 	export default {
 	  data () {
 	    return {
-	      tableData: [],
+	      userNumber:'',
+	      ticketNumber: ''
 	    }
 	  },
 	  created () {
-		
+	  	this.getData()
 	  },
 	  methods: {
-	  	
+	  	getData(){
+	  		this.$http.get('/query', { params: {}})
+	  			.then(res => {
+	  				if (res.body.ec == '200'){
+	  					console.log('res-->', res.body.data)
+	  					this.userNumber = res.body && res.body.data && res.body.data.usedNum
+	  					this.ticketNumber = res.body && res.body.data && res.body.data.alluserNum
+	  				} else {
+	  					this.$message.error(res.body.data);
+	  				}
+		      	});
+	  	}
 	  },
 	  components: {
 	  }
 	}
 </script>
+<style scoped lang='less'>
+	.container{
+		color:#606266;
+		margin-left:100px;
+		.titleUser{
+			margin-top:40px;
+			font-size: 14px;
+		}
+		.titleUserNumber{
+			margin-top:20px;
+			font-size: 40px;
+		}
+		.titleTicket{
+			margin-top:80px;
+			font-size: 14px;
+		}
+		.titleTicketNumber{
+			margin-top:20px;
+			font-size: 40px;
+		}
+	}
+</style>
