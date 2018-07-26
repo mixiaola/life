d660eb313fd76b4a80c3b8016cf4e053d4226709
peijiancheng <peijiancheng@didichuxing.com>
2018-07-26 10:43:45 +0800

    提交sql异常处理

--- app/src/module/sql.js
+++ app/src/module/sql.js
@@ -5,17 +5,34 @@ var mysql = require('mysql');
 //     password: '',
 //     database: 'life'
 // });
+var connection;
+//异常处理自动连接
+function handleError(err) {
+    if (err) {
+        // 如果是连接异常，自动重新连接
+        console.log('mysql err code:' + err.code);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR' || err.code === 'ETIMEDOUT') {
+            connect();
+        } else {
+            console.error(err.stack || err);
+        }
+    }
+}
 
-var connection = mysql.createConnection({
-    host: '39.105.23.187',
-    user: 'root',
-    port: '8080',
-    password: 'admin123',
-    database: 'life',
-    useConnectionPooling: true
-});
+function connect(){
+    connection = mysql.createConnection({
+        host: '39.105.23.187',
+        user: 'root',
+        port: '8080',
+        password: 'admin123',
+        database: 'life',
+        useConnectionPooling: true
+    });
+    connection.connect();
+    connection.on('error', handleError);
+}
 
-connection.connect();
+connect()
 
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
