2a00d9d53e25cea0710d2e08b6c94a615913b88a
peijiancheng <peijiancheng@didichuxing.com>
2018-08-14 19:11:49 +0800

    commit

---
+++ src/miniApp/pages/serach/serach.js
@@ -0,0 +1,78 @@
+// pages/serach/serach.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    shopList: [{
+      img: '../../img/item1.png',
+      title: '1231231',
+      content: '123123123',
+      time: '2019-29-10  2131-12-42',
+      address: '我哪知道在哪'
+    }, {
+      img: '../../img/item2.png',
+      title: '1231231',
+      content: '123123123',
+      time: '2019-29-10  2131-12-42',
+      address: '我哪知道在哪'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
